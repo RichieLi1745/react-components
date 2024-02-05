88d6e8822b822f3a95da7115bfccf74f921d60c5
Richard Li <richardli1745@gmail.com>
2024-02-05 12:13:47 -0800

    pass react-components

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
 // TODO
+const { useState } = React;
+const App = () => (
+  <div>
+    <h2>GroceryList</h2>
+    <GroceryList items={['Apples', 'Bananas']} />
+  </div>
+);
+//const GroceryListItem = (groceryList) => <li>{groceryList.item}</li>;
+const GroceryListItem = (groceryList) => {
+  const [grocery, setGrocery] = useState(false);
+
+  const style = {
+    fontWeight: grocery ? 'bold': 'normal',
+  };
+
+  return (
+    <li style={style} onMouseOver={() => setGrocery(true)} onMouseOut={()=> setGrocery(false)}>
+      {groceryList.item}
+    </li>
+  );
+};
+const GroceryList = (groceryList) => (
+  <ul>
+    {groceryList.items.map((item) => (
+      <GroceryListItem  item={item} />
+    ))}
+  </ul>
+);
+/*ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);*/
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
